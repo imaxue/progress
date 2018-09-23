de3d43b84fbc3f7374d9d79b3bf676997ad0fe12
cjjlovedsn <393801119@qq.com>
2018-09-23 21:58:22 +0800

    正则匹配优化

--- cjj/sublimeSnippetToVScode/index.js
+++ cjj/sublimeSnippetToVScode/index.js
@@ -1,33 +1,25 @@
 const fs = require('fs')
 const path = require('path')
 const util = require('util')
-
-const REG_BODY = /\[CDATA\[\s*([\s\t\S]*)\s*\]\]/
-const REG_PREFIX = /<tabTrigger>(.+)<\/tabTrigger>/
-const REG_DESC = /<description>(.+)<\/description>/
-
 const readdir = util.promisify(fs.readdir)
 const readFile = util.promisify(fs.readFile)
 const writeFile = util.promisify(fs.writeFile)
 const stat = util.promisify(fs.stat)
+const REG_RULE = /(?<=\[CDATA\[\s*)([\s\t\S]*)(?=\s*\]\])|(?<=<tabTrigger>)(.+)(?=<\/tabTrigger>)|(?<=<description>)(.+)(?=<\/description>)/g
 
 async function sublimeSnippetToVScode (src) {
   try {
     const result = {}
     const list = await readdir(src)
     for (const name of list) {
-      let prefix
-      let body
-      let description
       const filePath = path.join(src, name)
       const statIns = await stat(filePath)
       
       if (statIns.isDirectory()) continue
       const content = await readFile(filePath, { encoding: 'utf-8' })
-      prefix = REG_PREFIX.test(content) && RegExp.$1
-      body = REG_BODY.test(content) && RegExp.$1.split(/[\r\n]+/).filter(value => value) || ''
-      description = REG_DESC.test(content) && RegExp.$1 || ''
-      if (prefix) {
+      let [ body = '', prefix = '', description = '' ] = content.match(REG_RULE)
+      if (prefix && body) {
+        body = body.split(/[\r\n]+/).filter(value => value)
         result[prefix] = { prefix, body, description }
       }
     }
@@ -38,7 +30,7 @@ async function sublimeSnippetToVScode (src) {
 }
 
 sublimeSnippetToVScode(path.join(__dirname, 'src'))
-.then(result => writeFile('lua.json', JSON.stringify(result), { encoding: 'utf-8' }))
+.then(result => writeFile('snippet.json', JSON.stringify(result), { encoding: 'utf-8' }))
 .then(() => {
   console.log('Convert success!')
 }).catch(err => {
