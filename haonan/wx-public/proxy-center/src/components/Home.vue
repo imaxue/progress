d19e9f6c4a66057a0ed233e5b626ee1d10cab682
lijiaojiao02 <546890967@qq.com>
2018-11-29 21:26:22 +0800

    首页buju

--- haonan/wx-public/proxy-center/src/components/Home.vue
+++ haonan/wx-public/proxy-center/src/components/Home.vue
@@ -2,35 +2,42 @@
   <div class="home">
     <div class="top"></div>
     <div class="center">
+        <div class="title">
+            <div class="titleBlue"></div>
+            <div class="titleWhite">
+                <img  src="/static/images/icon.jpeg"/>
+                <!-- <div>姓名</div> -->
+            </div>
+        </div>
         <div class="center-content">
             <div class="weui-grids">
             <div class="weui-grid">
-                <div class="weui-grid__icon"> <img  src="/static/images/left1.png"/></div>
+                <div class="weui-grid__icon"> <img  src="/static/images/price.png"/></div>
                 <p class="weui-grid__label">我的佣金</p>
                 <p class="weui-grid__label">Grid</p>
             </div>
             <div class="weui-grid">
-                <div class="weui-grid__icon"><img  src="/static/images/right1.png"/></div>
+                <div class="weui-grid__icon"><img  src="/static/images/directPush.png"/></div>
                 <p class="weui-grid__label">我的直推</p>
                 <p class="weui-grid__label">Grid</p>
             </div>
             <div class="weui-grid">
-                <div class="weui-grid__icon"><img  src="/static/images/left2.png"/></div>
+                <div class="weui-grid__icon"><img  src="/static/images/cashDraw.png"/></div>
                 <p class="weui-grid__label">佣金提现</p>
                 <p class="weui-grid__label">Grid</p>
             </div>
             <div class="weui-grid">
-                <div class="weui-grid__icon"><img  src="/static/images/right2.png"/></div>
+                <div class="weui-grid__icon"><img  src="/static/images/secondTeam.png"/></div>
                 <p class="weui-grid__label">二级团队</p>
                 <p class="weui-grid__label">Grid</p>
             </div>
             <div class="weui-grid">
-                <div class="weui-grid__icon"><img  src="/static/images/left3.png"/></div>
+                <div class="weui-grid__icon"><img  src="/static/images/totalMoney.png"/></div>
                 <p class="weui-grid__label">我的总业绩</p>
                 <p class="weui-grid__label">Grid</p>
             </div>
             <div class="weui-grid">
-                <div class="weui-grid__icon"><img  src="/static/images/right3.png"/></div>
+                <div class="weui-grid__icon"><img  src="/static/images/teamSum.png"/></div>
                 <p class="weui-grid__label">团队总数</p>
                 <p class="weui-grid__label">Grid</p>
             </div>
@@ -64,6 +71,31 @@ export default {
   padding: 10px;
 }
 
+.title {
+    width: 100%;
+    margin-bottom: 50px;
+    .titleBlue {
+        height: 60px;
+        background-color: #10aeff;
+    }
+    .titleWhite{
+        background-color: white;
+        height: 80px;
+        margin: -30px 2%;
+        border-radius: 5px;
+        box-shadow: 2px 2px 2px 2px #cac6c6;
+    }
+    img{
+        width: 50px;
+        height: 50px;
+        border-radius: 25px;
+        position: relative;
+        left: 50%;
+        margin-left: -25px;
+        top:-15px;
+        
+    }
+}
 .center-content {
   border-radius: 10px;
   background-color: #fff;
