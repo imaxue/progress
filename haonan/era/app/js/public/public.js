ff2093d005c2433d97ebbb594dc3f58376a6dace
LHNfromTJ <Lhn930517>
2019-03-03 20:29:52 +0800

    设计公司

--- haonan/era/app/js/public/public.js
+++ haonan/era/app/js/public/public.js
@@ -1,7 +1,13 @@
 $(function () {
+    // body代理点击事件，点击其他地方隐藏头部下拉菜单
+    $('body').on('click', function (e) {
+        if (!$(e.target).parents().is('#headerCompany')) {
+            $('#headerCompanyMenu').slideUp();
+        }
+    });
     // 登录后头部头像点击显示下拉菜单
     $('#headerCompanyLogo').click(function () {
-        $('#headerCompanyMenu').slideDown();
+        $('#headerCompanyMenu').slideToggle();
     });
     // 点击“退出登录”
     $('#logout').click(function () {
