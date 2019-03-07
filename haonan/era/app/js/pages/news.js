3be2b87a60c41bc78ce62d60aac632961935e0ae
lihaonan04 <lihaonan04@58ganji.com>
2019-03-07 11:53:51 +0800

    轮播图差不多了

--- haonan/era/app/js/pages/news.js
+++ haonan/era/app/js/pages/news.js
@@ -1,45 +1,14 @@
 $(function () {
-    // swiper相关
-    new Swiper('#newsSwiper .swiper-container', {
-        watchSlidesProgress: true,
-        slidesPerView: 'auto',
-        centeredSlides: true,
-        loop: true,
+    $('.news-carousel').slick({
+        arrows: false,
+        dots: true,
+        centerMode: true,
+        slidesToShow: 1,
+        centerPadding: '120px',
         autoplay: false,
-        // slideToClickedSlide: true,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-        on: {
-            progress: function () {
-                for (i = 0; i < this.slides.length; i++) {
-                    var slide = this.slides.eq(i),
-                        slideProgress = this.slides[i].progress,
-                        modify = 1;
-                    if (Math.abs(slideProgress) > 1) {
-                        modify = (Math.abs(slideProgress) - 1) * 0.3 + 1;
-                    }
-                    var translate = slideProgress * modify * 260 + 'px',
-                        scale = 1 - Math.abs(slideProgress) / 5,
-                        zIndex = 999 - Math.abs(Math.round(10 * slideProgress));
-                    slide.transform('translateX(' + translate + ') scale(' + scale + ')');
-                    slide.css('zIndex', zIndex);
-                    slide.css('opacity', 1);
-                    if (Math.abs(slideProgress) > 3) {
-                        slide.css('opacity', 0);
-                    }
-                }
-            },
-            setTransition: function (transition) {
-                for (var i = 0; i < this.slides.length; i++) {
-                    var slide = this.slides.eq(i)
-                    slide.transition(transition);
-                }
-            }
-        }
+        autoplaySpeed: 3000,
+        draggable: false
     });
-
     // 遮罩相关
     $('.news-video-dialog .dialog-close').click(function () {
         $('body').css('overflow', 'auto');
