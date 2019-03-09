d95cf836307f64dcebae3b81b4e347539de02c85
junjun chen <393801119@qq.com>
2019-03-09 17:52:36 +0800

    表单验证

--- haonan/era/app/js/pages/account/profile.js
+++ haonan/era/app/js/pages/account/profile.js
@@ -1,13 +1,108 @@
 $(function () {
-  $('.submit').click(function () {
-    $('.submit--success').show()
+  jQuery.validator.setDefaults({
+    debug: true,
+    success: "valid"
+  });
+  jQuery.validator.addClassRules({
+
   })
-  $('.submit--success .confirm').click(function () {
-    $('.submit--success').hide()
-  })
-  var isValide = false
-  $('.invalid').toggle(!isValide)
-  $('.invalid .confirm').click(function () {
-    $('.invalid').hide()
-  })
+  var $doc = $(document)
+  var profile = {
+    state: {
+      isValid: false,
+      params: {
+        file: '',
+        fullname: '',
+        region: '',
+        province: '',
+        city: '',
+        town: ''
+      }
+    },
+
+    // 初始化
+    init: function () {
+      var _this = this
+      $('.submit').click(function () {
+        _this._submit()
+      })
+      this._dialogAction()
+      this._validate()
+      this._selectAction()
+    },
+
+    // 验证
+    _validate: function () {
+      // 此处发起请求验证是否认证通过,并更新状态
+      this.state.isValid = true
+      // 判断是否未认证，是的话弹出未认证提示对话框
+      $('.invalid').toggle(!this.state.isValid)
+      // 初始化表单验证
+      $('.infomation-edit>.form').validate({
+        rules: {
+          file: {
+            required: true,
+            accept: 'image/*'
+          },
+          fullname: {
+            required: true,
+            maxlength: 30
+          }
+        },
+        messages: {
+          file: '请上传头像',
+          fullname: {
+            required: '请输入公司全称',
+            maxlength: '最多输入30个字'
+          },
+          prop: '请选择公司属性',
+          region: '请选择地区',
+          province: '请选择省份',
+          city: '请选择城市',
+          town: '请选择地区',
+        }
+      })
+    },
+
+    //表单提交
+    _submit: function () {
+      // 验证表单
+      var valid = $('.infomation-edit>.form').valid()
+      console.log(valid)
+      if (valid) {
+
+        // 表单提交成功的提示
+        $('.submit--success').show()
+      }
+    },
+
+    // 注册下拉列表点击事件
+    _selectAction: function () {
+      var _this = this
+      $('.select').on('selected', function (e, params) {
+        var $this = $(this)
+        var $el = $this.children('input')
+        if (!$el) return
+        var name = $el.prop('name')
+        $this.children('.select__label').html(params.label)
+        $el.val(params.value)
+        _this.state.params[name] = params.value
+      })
+    },
+
+    // 注册弹框事件
+    _dialogAction: function () {
+      // 表单提交成功提示框关闭按钮
+      $('.submit--success .confirm').click(function () {
+        $('.submit--success').hide()
+      })
+      // 未认证弹框-关闭
+      $('.invalid .confirm').click(function () {
+        $('.invalid').hide()
+      })
+
+    }
+    // 未认证
+  }
+  profile.init()
 })
--- haonan/era/app/js/public/public.js
+++ haonan/era/app/js/public/public.js
@@ -1,4 +1,5 @@
 $(function () {
+    var $doc = $(document)
     // 头部栏
     // body代理点击事件，点击其他地方隐藏头部下拉菜单
     $('body').on('click', function (e) {
@@ -17,22 +18,33 @@ $(function () {
 
     // 下拉框
     // body代理点击事件，点击其他地方隐藏选项下拉菜单
-    $('body').on('click', function (e) {
-        if (!$(e.target).is('.select')) {
-            $('.select-options').slideUp();
-        }
+    $doc.on('click', function (e) {
+        $('.select').has('.select-options:visible').each(function () {
+            if (!this.contains(e.target)) {
+                $(this).children('.select-options').slideUp()
+            }
+        })
     });
     // 点击显示下拉框
-    $('.select').click(function () {
+    $('.select').click(function (e) {
         // 把其他的先收起
-        $(this).siblings().find('.select-options').slideUp('fast');
+        $('.select').not(this).find('.select-options').slideUp('fast');
         // 只展示当前的
-        $(this).children('.select-options').slideToggle('fast');
+        var $option = $(this).children('.select-options')
+        if ($option.get(0).contains(e.target)) return
+        $option.slideToggle('fast');
     });
     // 点击下拉框选项并收起下拉框
-    $('.select-options').on('click', function (e) {
+    $doc.on('click', '.select-option', function (e) {
         e.stopPropagation();
-        $(this).slideUp('fast');
+        var $this = $(this)
+        var value = $this.attr('data-value')
+        var $parent = $this.closest('.select-options')
+        $parent.slideUp('fast');
+        $this.closest('.select').trigger('selected', { label: $this.html(), value: value })
+
+    })
+    $('.select-options').on('click', function (e) {
     });
     // 已认证与未认证切换
     var isValide = true
