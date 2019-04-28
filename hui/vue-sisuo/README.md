7c39fb7bdf6b2d78027e043034457174854b8190
huangshenhui <huangshenhui>
2019-04-28 17:01:10 +0800

    写一个vue项目练手

---
+++ hui/vue-sisuo/README.md
@@ -0,0 +1,34 @@
+# sisuo
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn run serve
+```
+
+### Compiles and minifies for production
+```
+yarn run build
+```
+
+### Run your tests
+```
+yarn run test
+```
+
+### Lints and fixes files
+```
+yarn run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+```
+写一个vue项目练手   用vue-cli搭建  主用axios+element-ui+jquery+vue+vue-router
+
+```
---
+++ hui/vue-sisuo/src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">
+    <el-menu
+  :default-active="activeIndex"
+  class="el-menu-demo"
+  mode="horizontal"
+  @select="handleSelect"
+  background-color="#545c64"
+  text-color="#fff"
+  active-text-color="#ffd04b">
+  <el-menu-item index="unixbench">unixbench</el-menu-item>
+  <el-menu-item index="fio">fio</el-menu-item>
+  <el-menu-item index="netperf" >netperf</el-menu-item>
+  <el-menu-item index="stream" >stream</el-menu-item>
+  <el-menu-item index="pressure"><a href="https://www.baidu.com" target="_blank">压测系统</a></el-menu-item>
+    <el-menu-item index="Monitoring"><a href="https://www.baidu.com" target="_blank">监控系统</a></el-menu-item>
+</el-menu>
+    <Tabinfo :data="data"/>
+  </div>
+</template>
+
+<script>
+import Tabinfo from '@/components/Tabinfo.vue'
+export default {
+  name: "home",
+  components: {
+    Tabinfo
+  },
+  data() {
+    return {
+      activeIndex: 'unixbench',
+      data:"unixbench",
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+        this.data=key
+      },
+  }
+};
+</script>
